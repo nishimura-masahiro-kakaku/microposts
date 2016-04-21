efdcfc306b82dbf9821684b5354502f907304cd7
masahiro_nishimura <nishimura.masahiro.kakaku@gmail.com>
2016-04-21 05:46:15 +0000

    add edit_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate?, only: [:edit, :update, :destroy]
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,10 +20,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Update user information!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def authenticate?
+    if session[:user_id].to_i != params[:id].to_i
+      flash[:warning] = "Not authenticate"
+      redirect_to root_path
+    end
   end
 end
