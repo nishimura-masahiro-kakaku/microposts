d7335ea22b9c47be98929ae2278a088d070d47ca
masahiro_nishimura <nishimura.masahiro.kakaku@gmail.com>
2016-04-22 00:54:18 +0000

    add followings/followers

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,14 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-  
+
     config.generators do |g|
       g.stylesheets false
       g.javascript false
       g.helper false
       g.test_framework false
+
+      config.action_view.embed_authenticity_token_in_remote_forms = true
     end
   end
 end
